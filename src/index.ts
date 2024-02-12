488b9c6d179e738c77e6ee86a4bce5c2dffe2fa1
Paula Santamaría <m.paula.santamaria@gmail.com>
2024-02-12 20:28:34 -0300

    simplify listeners into a single handler

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,11 @@ import { Probot } from "probot";
 import IssueHaunter from "./issue-haunter";
 
 export = (app: Probot) => {
-    app.on("issues.opened", async (context) => {
-        return new IssueHaunter(context).invoke();
-    });
-
-    app.on("issues.closed", async (context) => {
-        return new IssueHaunter(context).invoke();
-    });
-
-    app.on("issues.assigned", async (context) => {
-        return new IssueHaunter(context).invoke();
-    });
-
-    app.on("issues.deleted", async (context) => {
-        return new IssueHaunter(context).invoke();
-    });
+    app.on([
+        "issues.opened",
+        "issues.closed",
+        "issues.assigned",
+        "issues.deleted"
+    ],
+        async (context) => new IssueHaunter(context).invoke());
 };
