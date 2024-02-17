5920e578ec1a8ba72ccc81dd32dcfe329b3613e4
Paula Santamaría <m.paula.santamaria@gmail.com>
2024-02-16 21:19:09 -0300

    use cwd instead of __dirname

--- src/ai/ghost-ai.ts
+++ src/ai/ghost-ai.ts
@@ -30,6 +30,6 @@ export async function getPrompt<TContext extends object>(
 async function getTemplate(
     templateName: string
 ): Promise<string> {
-    const filename = join(__dirname, 'prompt-templates', `${templateName}.txt`);
+    const filename = join(process.cwd(), 'prompt-templates', `${templateName}.txt`);
     return readFile(filename, 'utf8');
 }
