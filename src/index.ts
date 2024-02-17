318beefdb32f6ca67cac270d74f7ed72e7e2e3b8
Paula Santamaría <m.paula.santamaria@gmail.com>
2024-02-16 21:11:59 -0300

    test simple issue commenter deploy

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,26 @@
-import { Probot } from "probot";
-import IssueHaunter from "./issue-haunter";
-import IssueRewriter from "./issue-rewriter";
-import PullRequestHaunter from "./pullrequest-haunter";
+import { Context, Probot } from "probot";
+// import IssueHaunter from "./issue-haunter";
+// import IssueRewriter from "./issue-rewriter";
+// import PullRequestHaunter from "./pullrequest-haunter";
 
 export default (app: Probot) => {
     app.log.info("Ghost bot ready to haunt...");
 
-    app.on([
-        "issues.opened",
-        "issues.closed",
-        "issues.assigned",
-        "issues.deleted"
-    ],
-        async (context) => new IssueHaunter(context).invoke());
+    // app.on([
+    //     "issues.opened",
+    //     "issues.closed",
+    //     "issues.assigned",
+    //     "issues.deleted"
+    // ],
+    //     async (context) => new IssueHaunter(context).invoke());
 
-    app.on("issues.opened", async (context) => new IssueRewriter(context).invoke());
+    // app.on("issues.opened", async (context) => new IssueRewriter(context).invoke());
 
-    app.on("pull_request.opened", async (context) => new PullRequestHaunter(context).invoke());
+    // app.on("pull_request.opened", async (context) => new PullRequestHaunter(context).invoke());
+
+    app.on("issues.opened", async (context: Context) => {
+        return context.octokit.issues.createComment(
+            context.issue({ body: "Hello, World!" })
+        );
+    });
 };
